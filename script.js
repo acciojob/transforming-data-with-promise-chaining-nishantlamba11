7376664c258a048817695d3154ca62cccfbeec81
Nishant lamba <131985675+nishantlamba11@users.noreply.github.com>
2025-04-13 13:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //your JS code here. If required.
+let btn=document.querySelector("#btn");
+let output=document.querySelector("#output");
+let input=document.querySelector("#ip");
+
+btn.addEventListener("click",handleclick)
+
+function handleclick(){
+	let promise=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(input.value)
+		},2000)
+	})
+		promise.then(mul=>{
+		output.textContent=`Result:${mul}`
+			let multi=mul*2
+			return new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					resolve(multi)
+				},2000)
+			})
+	}).then(sub=>{
+			output.textContent=`Result:${sub}`
+			let subt=sub-3
+			return new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					resolve(subt)
+				},1000)
+			})
+	}).then(div=>{
+			output.textContent=`Result:${div}`
+			let dived=div/2
+			return new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					resolve(dived)
+				},1000)
+			})
+			
+	}).then(add=>{
+			output.textContent=`Result:${add}`
+			let added=add+10
+			return new Promise((resolve,reject)=>{
+				setTimeout(()=>{
+					resolve(added)
+				},1000)
+			})
+	}).then(final=>{
+			output.textContent=`Result:${final}`
+	})
+
+
+
+	
+}
